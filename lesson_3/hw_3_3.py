2d03339c20c9d76cb799c7692cd238f5b742d154
LNCP-W <niickriv@gmail.com>
2021-05-09 23:08:38 +0300

    чтение и запись файла

--- lesson_3/hw_3_3.py
+++ lesson_3/hw_3_3.py
@@ -5,17 +5,17 @@ def write_nub():
     f1.close()
 
 def translate_numb():
-    dictionary={'One':'Один', 'Two':'Два', 'Three':'Три', 'Four':'Четыре'}
-    f1=open('file_1.txt','r')
-    f2=open('file_2.txt','w')
-    flag=1
+    dictionary = {'One':'Один', 'Two':'Два', 'Three':'Три', 'Four':'Четыре'}
+    f1 = open('file_1.txt','r')
+    f2 = open('file_2.txt','w')
+    flag = 1
     while flag:
         line_f = f1.readline()
         if line_f != '':
-            word_old=line_f.split()[0]
-            line_f=line_f.replace(word_old, dictionary[word_old])
+            word_old = line_f.split()[0]
+            line_f = line_f.replace(word_old, dictionary[word_old])
             f2.write(line_f)
-        else: flag=0
+        else: flag = 0
     f1.close()
     f2.close()
 
