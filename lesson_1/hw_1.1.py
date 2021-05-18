b498f5963bc960557de9b8fdc149f463b4718428
Myroslav Hys <niickriv@gmail.com>
2021-05-19 01:10:30 +0300

    Исправлено

--- lesson_1/hw_1.1.py
+++ lesson_1/hw_1.1.py
@@ -2,14 +2,14 @@ def add_time (f, *args, **kwargs):
     import datetime
     def out (*fargs, **fkwargs):
         print (datetime.datetime.now())
-        n_f=f (*fargs, **fkwargs)
+        n_f = f (*fargs, **fkwargs)
         return n_f
     return out
 
 def user_adge (name = "Name",adge = "adge"):
     print (f'{name} is {adge} years old')
 
-user_age_with_time=add_time(user_adge)(name = "Name",adge = "adge")
+user_age_with_time = add_time(user_adge)(name = "Name",adge = "adge")
 
 @add_time
 def user_adge (name = "Name",adge = "adge"):
--- lesson_1/hw_1.3.py
+++ lesson_1/hw_1.3.py
@@ -1,7 +1,12 @@
+# def max_sum(a,b,c):
+#     x=[a,b,c]
+#     x.sort()
+#     max_sum=sum(x[1:3])
+#     return max_sum
+
 def max_sum(a,b,c):
     x=[a,b,c]
-    x.sort()
-    max_sum=sum(x[1:3])
-    return max_sum
+    x.remove(min(x))
+    return sum(x)
 
-print(max_sum(3,1,7))
+print(max_sum(5,1,7))
