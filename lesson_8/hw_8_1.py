68160972428595484dfe815348f65aca152d5f88
Myroslav Hys <niickriv@gmail.com>
2021-05-31 04:13:02 +0300

    Декоратор лога времени создания класа

--- lesson_8/hw_8_1.py
+++ lesson_8/hw_8_1.py
@@ -5,17 +5,18 @@ def time_log(f):
     def inner(*args, **kwargs):
         with open('test_file.txt', 'a', encoding='utf-8') as test_file:
             test_file.write(f'{datetime.now().strftime("%Y-%m-%d  %H:%M")} '
-                            f'создан екземпляр класса {f.__class__.__name__} '
+                            f'создан екземпляр класса {f.__name__} '
                             f'по адресу памяти {hex(id(f))}\n')
-        return f
-    return inner()
+        return f(*args, **kwargs)
+    return inner
 
-
+@time_log
 class SomeClass:
+
     def __init__(self, y):
         self.y = y
         print(self.y)
 
 
-x = time_log(SomeClass(63))
+x = SomeClass(y='55')
 
