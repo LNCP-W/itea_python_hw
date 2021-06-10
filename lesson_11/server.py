f367e3a45db84b30de8ecff1a72eb084eb36e4cc
LNCP-W <niickriv@gmail.com>
2021-06-11 00:31:40 +0300

    добавлена функция логирования

--- lesson_11/server.py
+++ lesson_11/server.py
@@ -1,17 +1,18 @@
 import socket
+from datetime import datetime
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("127.0.0.1", 10002))
 sock.listen(socket.SOMAXCONN)
 conn, addr = sock.accept()
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        print("Нет входящих данных")
-        break
-    with open("skorogovorka_new.txt", "a", encoding="utf-8") as f:
-        f.write(data.decode("utf-8"))
+with open("skorogovorka_new.txt", "a", encoding="utf-8") as f:
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print("Нет входящих данных")
+            break
+        f.write(f"{datetime.now()} {data.decode('utf - 8')}")
 
 conn.close()
 sock.close()
