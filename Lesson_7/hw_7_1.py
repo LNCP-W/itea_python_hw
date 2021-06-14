1e6a2f18f613c25bf2cd9b28d30d39ed8a9430db
LNCP-W <niickriv@gmail.com>
2021-06-14 16:52:48 +0300

    исправлено

--- Lesson_7/hw_7_1.py
+++ Lesson_7/hw_7_1.py
@@ -8,12 +8,12 @@ conn_str = Env().str('db_conn_str')
 conn = psycopg2.connect(conn_str)
 
 
-class StandartMetod:
+class StandartModel:
 
     def delete_one(self, table, some_id, id_value):
-        data_insert = f"""DELETE FROM {table} WHERE {some_id} = {id_value}"""
+        data_insert_del = f"""DELETE FROM {table} WHERE {some_id} = {id_value}"""
         with conn, conn.cursor() as cursor:
-            cursor.execute(data_insert)
+            cursor.execute(data_insert_del)
 
     def _change_some(self, table, name_field, id_field, some_id, new_name):
         data_insert = f""" UPDATE {table} SET {name_field} = '{new_name}' WHERE {id_field} = {some_id}"""
@@ -28,6 +28,14 @@ class StandartMetod:
 
 
 class Order(StandartMetod):
+    data_insert_create = """
+                INSERT INTO orders 
+                (created_dt, order_type, description, serial_no, creator_id, status)
+                VALUES (%s, %s, %s, %s, %s, %s) 
+                RETURNING order_id
+                """
+    data_insert_change_desc = """UPDATE orders SET description = %s, updated_dt = %s WHERE order_id=%s"""
+    data_insert_change_creator = """UPDATE orders SET creator_id = %s, updated_dt = %s WHERE order_id = %s"""
 
     def __init__(self, order_type, description, serial, creator_id, status='new'):
         self.status = status
@@ -37,22 +45,16 @@ class Order(StandartMetod):
         self.serial = serial
         self.__id = uuid4().hex
         self.time_create = date.today()
-        data_insert = """
-            INSERT INTO orders 
-            (created_dt, order_type, description, serial_no, creator_id, status)
-            VALUES (%s, %s, %s, %s, %s, %s) 
-            RETURNING order_id
-            """
         fields = (self.time_create, self.order_type, self.description, self.serial, self.creator_id, self.status)
         with conn, conn.cursor() as cursor:
-            cursor.execute(data_insert, fields)
+            cursor.execute(data_insert_create, fields)
             self.order_id = cursor.fetchone()[0]
 
     def in_active(self):
         if self.status == "not_active":
-            return "This Order not active"
+            print ("This Order not active")
         x = date.today() - self.time_create
-        return f"Order is active {x.days} days."
+        return x.days
 
     def change_status(self, new_status):
         self.status = new_status
@@ -60,17 +62,15 @@ class Order(StandartMetod):
 
     def change_description(self, new_description):
         self.description = new_description
-        data_insert = """UPDATE orders SET description = %s, updated_dt = %s WHERE order_id=%s"""
         fields = (self.description, date.today(), self.order_id)
         with conn, conn.cursor() as cursor:
-            cursor.execute(data_insert, fields)
+            cursor.execute(data_insert_change_desc, fields)
 
     def changr_creator(self, new_creator):
         self.creator_id = new_creator
-        data_insert = """UPDATE orders SET creator_id = %s, updated_dt = %s WHERE order_id = %s"""
         fields = (new_creator, date.today(), self.order_id)
         with conn, conn.cursor() as cursor:
-            cursor.execute(data_insert, fields)
+            cursor.execute(data_insert_change_creator, fields)
 
     def order_id(self):
         return f"Order id is: {self.__id}"
@@ -80,15 +80,15 @@ class Order(StandartMetod):
 
 
 class Department(StandartMetod):
+    data_insert_create = """
+            INSERT INTO  departments (department_name) 
+            VALUES (%s) RETURNING department_id
+            """
 
     def __init__(self, department_name):
         self.name = department_name
-        data_insert = """
-        INSERT INTO  departments (department_name) 
-        VALUES (%s) RETURNING department_id
-        """
         with conn, conn.cursor() as cursor:
-            cursor.execute(data_insert, (department_name, ))
+            cursor.execute(data_insert_create, (department_name, ))
             self.department_id = cursor.fetchone()[0]
 
     def change_name(self, new_name):
@@ -99,19 +99,19 @@ class Department(StandartMetod):
 
 
 class Employees(StandartMetod):
+    data_insert_create = """
+        INSERT INTO employees (fio, position, department_id) 
+        VALUES (%s, %s, %s) 
+        RETURNING employee_id
+        """
 
     def __init__(self,  fio, position, department_id):
         self.fio = fio
         self.position = position
         self.department_id = department_id
         with conn, conn.cursor() as cursor:
-            data_insert = """
-                INSERT INTO employees (fio, position, department_id) 
-                VALUES (%s, %s, %s) 
-                RETURNING employee_id
-                """
             fields = (self.fio, self.position, self.department_id)
-            cursor.execute(data_insert, fields)
+            cursor.execute(data_insert_create, fields)
             self.employee_id = cursor.fetchone()[0]
 
     def change_name(self, new_name):
