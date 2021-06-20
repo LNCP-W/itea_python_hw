8c83730f20e4bfb0a0cb590397729db94eaac557
LNCP-W <niickriv@gmail.com>
2021-06-20 23:27:41 +0300

    исправлено

--- lesson_12/hw_12_2.py
+++ lesson_12/hw_12_2.py
@@ -18,7 +18,8 @@
 #         return f"Department {self.dep_name} located in {self.location}"
 #
 #     def __repr__(self):
-#         return f"{self.dep_name} in {self.location}"
+#         res = f"\"id\": \"{self.__getitem__('id')}\", \"name\": \"{self.dep_name}\", \"location\": \"{self.location}\""
+#         return "{" + res + "}"
 #
 #     def write_to_json(self):
 #         json_data = self.to_json()
@@ -61,7 +62,11 @@
 #                f"in department {self.department_id}"
 #
 #     def __repr__(self):
-#         return f"{self.fio} {self.position} in {self.department_id}"
+#         res = f"\"id\": \"{self.__getitem__('id')}\", " \
+#               f"\"name\": \"{self.fio}\", " \
+#               f"\"position\": \"{self.position}\", " \
+#               f"\"department\": \"{self.department_id}\""
+#         return "{" + res + "}"
 #
 #     def write_to_json(self):
 #         json_data = self.to_json()
@@ -109,9 +114,15 @@
 #                f"serial number: {self.serial}, last update: {self.updated}."
 #
 #     def __repr__(self):
-#         return f"\'date\': \'{str(self.time_create)}\', \'employee\': \'{str(self.order_creator)}\', " \
-#                f"\'status\':\'{self.order_status}\', \'type\': \'{self.order_type}\', \'description\': \'{self.description}\', " \
-#                f"\'serial number\': \'{self.serial}\', \'updated\': \'{str(self.updated)}\'"
+#         res = f"\"id\": \"{self.__getitem__('id')}\", " \
+#               f"\"date\": \"{str(self.time_create.strftime('%d.%m.%Y %H:%M'))}\", " \
+#               f"\"employee\": \"{self.order_creator.fio}\", " \
+#               f"\"status\":\"{self.order_status}\", " \
+#               f"\"type\": \"{self.order_type}\", " \
+#               f"\"description\": \"{self.description}\", " \
+#               f"\"serial number\": \"{self.serial}\", " \
+#               f"\"updated\": \"{str(self.updated.strftime('%d.%m.%Y %H:%M'))}\""
+#         return "{" + res + "}"
 #
 #     def save(self, *args, **kwargs):
 #         self.time_create = datetime.now()
@@ -152,19 +163,82 @@
 # def search_order(upd_id):
 #     return f'{Orders.objects(id=upd_id)} finded'
 
+"""input:
+    [
+    "60bff4aeb95d043f407d0f3d",
+    "60c266067725e4e3242e7a8c",
+    "60c266a97725e4e3242e7a8d",
+    "60c26dd5926f2d9b2c8b5597",
+    "60c2719e2b13b1872cfbf972"
+    ]
+    """
+
 @my_app.route('/search_orders', methods=["POST"])
 def search_orders():
-    input_data=json.loads(request.data)
+    input_data = json.loads(request.data)
     data = []
     def f(oid):
-        x="{"+str(Orders.objects(id=oid))[1:-1]+"}"
-        x=json.loads(x.replace("'",'"'))
+        x = json.loads(str(Orders.objects(id=oid))[1:-1])
         data.append(x)
     with ThreadPoolExecutor(max_workers=len(input_data)) as pool:
         for i in input_data:
-            pool.submit(f,i)
+            pool.submit(f, i)
     return json.dumps(data, sort_keys=True, indent=4)
 
+
+"""output:
+[
+{
+"date": "19.06.2021 16:45",
+"description": "some1desc",
+"employee": "fifo",
+"id": "60c266067725e4e3242e7a8c",
+"serial number": "25252525",
+"status": "status",
+"type": "o_type",
+"updated": "10.06.2021 23:17"
+},
+{
+"date": "19.06.2021 16:45",
+"description": "asdmmmmmmesc",
+"employee": "fifo",
+"id": "60bff4aeb95d043f407d0f3d",
+"serial number": "555",
+"status": "ddsljgtmatus",
+"type": "45322e",
+"updated": "10.06.2021 23:17"
+},
+{
+"date": "19.06.2021 16:45",
+"description": "87654321",
+"employee": "fifo",
+"id": "60c2719e2b13b1872cfbf972",
+"serial number": "25252525",
+"status": "sssss",
+"type": "ddddd",
+"updated": "10.06.2021 23:17"
+},
+{
+"date": "19.06.2021 16:45",
+"description": "some1desc",
+"employee": "fifo",
+"id": "60c26dd5926f2d9b2c8b5597",
+"serial number": "25252525",
+"status": "swwwstatus",
+"type": "o_tysdpe",
+"updated": "10.06.2021 23:17"
+},
+{
+"date": "19.06.2021 16:45",
+"description": "some1desc",
+"employee": "fifo",
+"id": "60c266a97725e4e3242e7a8d",
+"serial number": "25252525",
+"status": "sdstatus",
+"type": "o_tysdpe",
+"updated": "10.06.2021 23:17"
+}
+]"""
 
 
 # my_app.run()
