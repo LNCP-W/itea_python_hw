22a047453de3aa7e802accdc66a5f7c88781a56f
LNCP-W <niickriv@gmail.com>
2021-06-29 22:47:42 +0300

    some

--- lesson_15/main.py
+++ lesson_15/main.py
@@ -1,19 +1,43 @@
+import datetime
+import json
+
 from models import app, db, Departments, Employees, Clients, Orders
 from flask import Flask, request, render_template
 
 @app.route('/search', methods=["GET"])
 def search():
-    page_title = "Результат поиска"
-    for x in request.args.items():  # /search?Сотрудники=60bfe821f98dbc8e792f900b
+    for x in request.args.items():
         dict_do = {
-            'Заявки': Orders.query.filter_by(o_id=x[1]).first(),
-            'Сотрудники': Employees.query.filter_by(e_id=x[1]).first(),
-            'Департаменты': Departments.query.filter_by(d_id=x[1]).first()
+            'Заявки': [Orders.query.filter_by(id=x[1]).first(), 1, 'order.html'],
+            'Сотрудники': [
+                        Employees.query.filter_by(id=x[1]).first(),
+                        Orders.query.filter_by(creator=x[1]).limit(10).all(),
+                        'employee.html'
+                        ],
+            'Департаменты': [
+                        Departments.query.filter_by(id=x[1]).first(),
+                        Employees.query.filter_by(dep_id=x[1]).limit(10).all(),
+                        'department.html'
+                        ],
+            'Клиенты': [
+                        Clients.query.filter_by(id=x[1]).first(),
+                        Orders.query.filter_by(client=x[1]).limit(10).all(),
+                        'client.html'
+                        ]
             }
-    return render_template('search_result.html', title=page_title, results=dict_do[x[0]])
-    # except mongoengine.errors.ValidationError:
-    #     return render_template('search_result.html', title=page_title, results='Нет такой заявки')
+    res=dict_do[x[0]]
 
+    if not res[0]:
+        res= "Нет такого id"
+        return render_template('index.html', title='Главная страница', results=res)
+    return render_template(res[2], title=res[0].id, results=res)
+
+
+@app.route('/all_clients')
+def all_clients():
+    data = Clients.query.limit(10).all()
+    page_title = "Клиенты"
+    return render_template('clients.html', title=page_title, results=data)
 
 @app.route('/all_orders')
 def all_orders():
@@ -34,6 +58,78 @@ def all_departments():
     data = Departments.query.limit(10).all()
     page_title = 'Департаменты'
     return render_template('departments.html', title=page_title, results=data)
+
+@app.route('/edit_department', methods=["GET"])
+def edit_department():
+    dep = Departments.query.filter_by(id=request.args["id"]).first()
+    dep.name = request.args["name"]
+    dep.location = request.args["location"]
+    dep.phone = request.args["phone"]
+    db.session.commit()
+    request.args={"Департаменты":request.args["id"]}
+    return search()
+
+@app.route('/edit_employee', methods=["GET"])
+def edit_employee():
+    emp = Employees.query.filter_by(id=request.args["id"]).first()
+    emp.name = request.args["name"]
+    emp.position = request.args["position"]
+    emp.phone = request.args["phone"]
+    emp.dep_id = request.args["dep_id"]
+    db.session.commit()
+    request.args={"Сотрудники": request.args["id"]}
+    return search()
+
+@app.route('/edit_order', methods=["GET"])
+def edit_order():
+    ord = Orders.query.filter_by(id=request.args["id"]).first()
+    ord.creator = request.args["creator"]
+    ord.status = request.args["status"]
+    ord.type = request.args["type"]
+    ord.descript = request.args["descript"]
+    ord.serial = request.args["serial"]
+    ord.price = request.args["price"]
+    ord.updated = datetime.datetime.now()
+    db.session.commit()
+    request.args={"Заявки": request.args["id"]}
+    return search()
+
+@app.route("/create_dep")
+def create_dep():
+    if not request.args:
+        page_title = 'Cоздание департамента'
+        return render_template('create_dep.html', title=page_title)
+    else:
+        new_dep = Departments(**request.args)
+        db.session.add(new_dep)
+        db.session.commit()
+        request.args = {"Департаменты": new_dep.id}
+        return search()
+
+@app.route("/create_emp")
+def create_emp():
+    if len(request.args) < 2:
+        page_title = 'Регистрация Сотрудника '
+        return render_template('create_emp.html', title=page_title, id=request.args['dep_id'])
+    else:
+        new_emp = Employees(**request.args)
+        db.session.add(new_emp)
+        db.session.commit()
+        request.args = {"Сотрудники": new_emp.id}
+        return search()
+
+@app.route("/create_ord")
+def create_ord():
+    if len(request.args) < 2:
+        page_title = 'Регистрация Заявки'
+        return render_template('create_order.html', title=page_title, creator=request.args['creator'])
+    else:
+        date={'created':datetime.datetime.now(), 'updated':datetime.datetime.now()}
+        new_ord = Orders(**date.update(**request.args))
+        db.session.add(new_ord)
+        db.session.commit()
+        request.args = {"Заявки": new_ord.id}
+        return search()
 
 
 @app.route("/index")
--- lesson_15/models.py
+++ lesson_15/models.py
@@ -7,94 +7,97 @@ db = SQLAlchemy(app)
 
 
 class Departments(db.Model):
-    d_id = db.Column(db.Integer, primary_key=True)
-    d_name = db.Column(db.String(50), unique=True, nullable=False)
-    d_location = db.Column(db.String(50), nullable=False)
-    d_phone = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.Integer)
 
     def __str__(self):
-        return f"Department {self.d_name} with id:{self.d_id} located in {self.d_location}"
+        return f"Department {self.name} with id:{self.id} located in {self.location}"
 
     def __repr__(self):
         res = {
-            "id": self.d_id,
-            "name": self.d_name,
-            "location": self.d_location,
-            "phone": self.d_phone
+            "id": self.id,
+            "name": self.name,
+            "location": self.location,
+            "phone": self.phone
             }
         return str(res)
 
 
 class Employees(db.Model):
-    e_id = db.Column(db.Integer, primary_key=True)
-    e_name = db.Column(db.String(50), unique=True, nullable=False)
-    e_position = db.Column(db.String(50), nullable=False)
-    e_phone = db.Column(db.Integer, nullable=False)
-    e_dep_id = db.Column(db.Integer, db.ForeignKey('departments.d_id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    dep_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
 
     def __str__(self):
-        return f"Employee {self.e_name} with id:{self.e_id} worked on position {self.e_position} " \
-               f"in department {self.e_dep_id}, phone number: {self.e_phone}."
+        return f"Employee {self.name} with id:{self.id} worked on position {self.position} " \
+               f"in department {self.dep_id}, phone number: {self.phone}."
 
     def __repr__(self):
         res = {
-               "id": self.e_id,
-               "name": self.e_name,
-               "position": self.e_position,
-               "phone": self.e_phone,
-               "department_id": self.e_dep_id
+               "id": self.id,
+               "name": self.name,
+               "position": self.position,
+               "phone": self.phone,
+               "department_id": self.dep_id
                }
         return str(res)
 
 
 class Clients(db.Model):
-    c_id = db.Column(db.Integer, primary_key=True)
-    c_name = db.Column(db.String(50), unique=True, nullable=False)
-    c_phone = db.Column(db.Integer, nullable=False)
-    c_is_problem = db.Column(db.Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    is_problem = db.Column(db.Boolean, default=False)
 
     def __str__(self):
-        return f"Client {self.c_name}{'!!!'*bool(self.c_is_problem)} with id:{self.c_id}, phone number: {self.c_phone}."
+        return f"Client {self.name}{'!!!'*bool(self.is_problem)} with id:{self.id}, phone number: {self.phone}."
 
     def __repr__(self):
         res = {
-               "id": self.c_id,
-               "name": self.c_name,
-               "phone": self.c_phone,
-               "problem":self.c_is_problem
+               "id": self.id,
+               "name": self.name,
+               "phone": self.phone,
+               "problem": str(self.is_problem)
                }
         return str(res)
 
 
 class Orders(db.Model):
-    o_id = db.Column(db.Integer, primary_key=True)
-    o_created = db.Column(db.DateTime)
-    o_status = db.Column(db.String(50), nullable=False)
-    o_type = db.Column(db.String(50), nullable=False)
-    o_descript = db.Column(db.String(200), nullable=False)
-    o_creator = db.Column(db.Integer, db.ForeignKey("employees.e_id"), nullable=False)
-    o_serial = db.Column(db.String(20), nullable=False)
-    o_price = db.Column(db.Integer)
-    o_client = db.Column(db.Integer, db.ForeignKey("clients.c_id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime)
+    status = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    descript = db.Column(db.String(200), nullable=False)
+    creator = db.Column(db.Integer, db.ForeignKey("employees.id"), nullable=False)
+    serial = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Integer, default=0)
+    client = db.Column(db.Integer, db.ForeignKey("clients.id"), nullable=False)
+    updated = db.Column(db.DateTime)
 
     def __str__(self):
-        return f"Order was created {self.o_created} by employee {str(self.o_creator)}. " \
-               f"Actual status is {self.o_status}, type: {self.o_type}, description: {self.o_descript}, " \
-               f"serial number: {self.o_serial}, last update: ."
+        return f"Order was created {self.created} by employee {str(self.creator)}. " \
+               f"Actual status is {self.status}, type: {self.type}, description: {self.descript}, " \
+               f"serial number: {self.serial}, last update: ."
 
     def __repr__(self):
+        price = self.price
+        if not self.price: price = ''
         res = {
-              "id": {self.o_id},
-              "created": {self.o_created},
-              "status": {self.o_status},
-              "type": {self.o_type},
-              "description": {self.o_descript},
-              "creator": {self.o_creator},
-              "serial": {self.o_serial},
-              "price": {self.o_price},
-              "client": {self.o_client}
+              "id": self.id,
+              "created": self.created.strftime('%d.%m.%Y %H:%M'),
+              "status": self.status,
+              "type": self.type,
+              "description": self.descript,
+              "creator": self.creator,
+              "serial": self.serial,
+              "price": price,
+              "client": self.client
               }
         return str(res)
 
 
-# db.create_all()
+# db.creatall()
