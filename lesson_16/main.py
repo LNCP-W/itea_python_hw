83430ae20d7a4321ee54af89cd16a38fe5a85216
LNCP-W <niickriv@gmail.com>
2021-07-09 00:32:48 +0300

    rabbit

--- lesson_16/main.py
+++ lesson_16/main.py
@@ -3,101 +3,6 @@ from models import app, db, Departments, Employees, Customers, Orders
 from flask import request, render_template
 
 
-@app.route('/search', methods=["GET"])
-def search():
-    for x in request.args.items():
-        dict_do = {
-            'Заявки': [Orders.query.filter_by(id=x[1]).first(), 1, 'order.html'],
-            'Сотрудники': [
-                        Employees.query.filter_by(id=x[1]).first(),
-                        Orders.query.filter_by(creator=x[1]).limit(10).all(),
-                        'employee.html'
-                        ],
-            'Департаменты': [
-                        Departments.query.filter_by(id=x[1]).first(),
-                        Employees.query.filter_by(dep_id=x[1]).limit(10).all(),
-                        'department.html'
-                        ],
-            'Клиенты': [
-                        Customers.query.filter_by(id=x[1]).first(),
-                        Orders.query.filter_by(customer=x[1]).limit(10).all(),
-                        'customer.html'
-                        ]
-            }
-    res = dict_do[x[0]]
-
-    if not res[0]:
-        res = "Нет такого id"
-        return render_template('index.html', title='Главная страница', results=res)
-    return render_template(res[2], title=res[0].id, results=res)
-
-
-@app.route('/all_customers')
-def all_customers():
-    data = Customers.query.limit(10).all()
-    page_title = "Клиенты"
-    return render_template('customers.html', title=page_title, results=data)
-
-
-@app.route('/all_orders')
-def all_orders():
-    data = Orders.query.limit(10).all()
-    page_title = "Заявки"
-    return render_template('orders.html', title=page_title, results=data)
-
-
-@app.route('/all_employees')
-def all_employees():
-    data = Employees.query.limit(10).all()
-    page_title = 'Сотрудники'
-    return render_template('employees.html', title=page_title, results=data)
-
-
-@app.route('/all_departments')
-def all_departments():
-    data = Departments.query.limit(10).all()
-    page_title = 'Департаменты'
-    return render_template('departments.html', title=page_title, results=data)
-
-
-@app.route('/edit_department', methods=["GET"])
-def edit_department():
-    dep = Departments.query.filter_by(id=request.args["id"]).first()
-    dep.name = request.args["name"]
-    dep.location = request.args["location"]
-    dep.phone = request.args["phone"]
-    db.session.commit()
-    request.args = {"Департаменты": request.args["id"]}
-    return search()
-
-
-@app.route('/edit_employee', methods=["GET"])
-def edit_employee():
-    emp = Employees.query.filter_by(id=request.args["id"]).first()
-    emp.name = request.args["name"]
-    emp.position = request.args["position"]
-    emp.phone = request.args["phone"]
-    emp.dep_id = request.args["dep_id"]
-    db.session.commit()
-    request.args = {"Сотрудники": request.args["id"]}
-    return search()
-
-
-@app.route('/edit_order', methods=["GET"])
-def edit_order():
-    order = Orders.query.filter_by(id=request.args["id"]).first()
-    order.creator = request.args["creator"]
-    order.status = request.args["status"]
-    order.type = request.args["type"]
-    order.descript = request.args["descript"]
-    order.serial = request.args["serial"]
-    order.price = request.args["price"]
-    order.updated = datetime.datetime.now()
-    db.session.commit()
-    request.args = {"Заявки": request.args["id"]}
-    return search()
-
-
 @app.route("/create_dep")
 def create_dep():
     if not request.args:
@@ -161,27 +66,6 @@ def create_ord():
         request.args = {"Заявки": new_ord.id}
         return search()
 
-
-@app.route("/delete_dep")
-def delete_dep():
-    dep = Departments.query.filter_by(id=request.args['id']).first()
-    db.session.delete(dep)
-    db.session.commit()
-    return all_departments()
-
-
-@app.route("/delete_emp")
-def delete_emp():
-    dep = Employees.query.filter_by(id=request.args['id']).first()
-    db.session.delete(dep)
-    db.session.commit()
-    return all_employees()
-
-
-@app.route("/index")
-def index():
-    page_title = 'Главная страница'
-    return render_template('index.html', title=page_title)
 
 
 if __name__ == "__main__":
